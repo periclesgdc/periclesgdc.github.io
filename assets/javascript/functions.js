b7c961cd8a48eee3c82888f613b6dbc85a23e128
periclesgdc <periclesgdc@gmail.com>
2017-08-21 15:17:38 -0300

    Atualizando

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -2,8 +2,24 @@ $(document).ready(function(){
 	var lastItem = $("#menu ul li:nth-child(1)");
 
 	$("#menu ul li").mouseover(function() {
-		lastItem.fadeOut().fadeIn().show().attr('class', 'light-blue');
-		$(this).fadeOut().fadeIn().show().attr('class', 'dark-blue');
+		lastItem.fadeOut(300, 'swing', lastItem.attr('class', 'light-blue')).fadeIn();
+		$(this).fadeOut(300, 'swing', $(this).attr('class', 'dark-blue')).fadeIn();
 		lastItem = $(this);
 	});
 });
+
+function callAjax(page) {
+	$.ajax({
+    type: 'POST',
+    url: ''+page+'.html',               
+    cache: false,
+    success: function(result) {
+        if(result){
+            $("content").html(result);
+        } 
+        else {
+            alert("Erro");
+        }
+    }
+});
+}
